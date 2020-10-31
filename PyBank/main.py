bc4574f05fd3d790b037c53ebef1672fa4bf125d
Leonard Zhao <lzhao178@gmail.com>
2020-10-30 22:11:14 -0700

    added script for avg change, greatest increase/decrease

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,6 +7,13 @@ bank_csv = os.path.join("Resources", "budget_data.csv")
 #defining variables
 total_months = 0
 total_profits = 0
+total_profit_change = 0
+last_profit = 0
+highest_profits = -999999999
+lowest_profits = 999999999
+highest_month = ""
+lowest_month = ""
+
 
 #read in csv file
 with open (bank_csv, "r") as csvfile:
@@ -14,12 +21,30 @@ with open (bank_csv, "r") as csvfile:
     header = next(csvreader)
 
     for row in csvreader:
-        month = row[0]
-        total_months += 1 
         profit = int(row[1])
-        total_profits = total_profits + profit
+        month = row[0]
+        if total_months == 0:
+            last_profit = profit
+        else:
+            total_profit_change = profit - last_profit
+            last_profit = profit
+        if profit > highest_profits:
+            highest_profits = profit
+            highest_month = row[0]
+        if profit < lowest_profits:
+            lowest_profits = profit
+            lowest_month = row[0]
 
-print(f"Total Months: {total_months}")
-print(f"Net Total Profit: ${total_profits:0,.0f}")
+        total_months += 1
+        total_profits = total_profits + profit
+        avg_change = total_profit_change/total_months -1
+
+    print(f"Total Months: {total_months}")
+    print(f"Net Total Profit: ${total_profits:0,.0f}")
+    print(f"Average Change: ${avg_change:0,.0f}")
+    print(f"Greatest Increase: {highest_month} ${highest_profits}")
+    print(f"Greatest Decrease: {lowest_month} ${lowest_profits}")
+    
+   
     
 
