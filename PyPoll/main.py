6bf76f00221da10f0caabf28552b74d707c20c3c
Leonard Zhao <lzhao178@gmail.com>
2020-11-02 16:47:06 -0800

    updated code to show winner

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -24,16 +24,39 @@ with open (poll_csv, "r") as csvfile:
         else:
             candidate_count[candidates_list.index(candidate)] += 1
 
+#percentage 
 def percentage(parameter):
     percent = (parameter/total_votes) * 100     
     return (percent)
-    
+
+
+most_votes = candidate_count[0]
+most_votes_count = 0
+for x in range(len(candidates_list)):
+    if candidate_count[x]> most_votes:
+        most_votes_count = x
+winner = candidates_list[most_votes_count]
+           
 print("Election Results")
 print("---------------------------")    
 print(f"Total Votes: {total_votes}")
 print("---------------------------")
 for x in range(len(candidates_list)):
     print (f"{candidates_list[x]}: {percentage(candidate_count[x]):.2f}% ({candidate_count[x]})")
-    
+print("---------------------------")
+print(f"Winner: {winner}") 
+print("---------------------------")
 
-    
+#write to txt file
+output_file = os.path.join("Analysis", "poll_analysis.txt")
+
+with open(output_file, 'w', newline='') as txtfile:
+    txtfile.write("Election Results\n")
+    txtfile.write("---------------------------\n")
+    txtfile.write(f"Total Votes: {total_votes}\n")
+    txtfile.write("---------------------------\n")
+    for x in range(len(candidates_list)):
+        txtfile.write(f"{candidates_list[x]}: {percentage(candidate_count[x]):.2f}% ({candidate_count[x]})\n")
+    txtfile.write("-----------------------------\n")
+    txtfile.write(f"Winner: {winner}\n")
+    txtfile.write("-----------------------------\n")
